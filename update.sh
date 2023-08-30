2f32371860b6297920251547378e17d7b4929eef
Jonas <sturmente@proton.me>
2023-08-30 23:00:16 +0200

    Added build scripts and files

---
+++ update.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+echo "\033[104mStarting update\033[0m"
+echo "\033[104mPull from GitHub\033[0m"
+git pull
+echo "\033[104mInstall npm packages\033[0m"
+npm i
+echo "\033[104mBuild application\033[0m"
+npm run build
+cd build
+echo "\033[104mBuild Docker image\033[0m"
+docker build -t guild-icon-roulette .
+echo "\033[104mUpdated\033[0m"
