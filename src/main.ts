366c872cd6060cd3a6a10249931960f1fa3e2601
Jonas <sturmente@proton.me>
2023-08-30 22:13:19 +0200

    Get images from image channel

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,52 @@
 import "dotenv/config";
-import { Client } from "discord.js";
+import { Client, ChannelType, Message } from "discord.js";
+
+const config = require("../config.json");
 
 const client: Client = new Client({ intents: ["Guilds"] });
 
 client.on("ready", () => {
 	console.log("Client logged in as", client.user?.tag);
+
+	changeImage();
 });
+
+async function changeImage() {
+	const guild = await client.guilds.fetch(config.guild_id);
+	const channel = await guild.channels.fetch(config.image_channel_id);
+
+	if (!channel) {
+		console.log("Channel not found");
+		return;
+	}
+
+	if (channel.type != ChannelType.GuildText) {
+		console.log("Channel has wrong type");
+		return;
+	}
+
+	let images = new Array<string>();
+
+	let options = {};
+
+	while (true) {
+		const newMessages = await channel.messages.fetch(options);
+
+		if (!newMessages.last()) {
+			break;
+		}
+
+		newMessages.forEach((message) => {
+			const attachments = message.attachments.filter((attachment) => attachment.contentType?.startsWith("image"));
+			attachments.forEach((attachment) => {
+				images.push(attachment.url);
+			});
+		});
+
+		options = { before: (<Message>newMessages.last()).id };
+	}
+
+	console.log(images);
+}
 
 client.login(process.env.TOKEN);
